505a192ec785cdf6cc2c4a71497faf3535f2507c
marcindluzewski <102620318+marcindluzewski@users.noreply.github.com>
2022-04-19 19:54:04 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Marcin";
+        int year = 2022;
+        double salary = 1800.26;
+        String month = "April";
+        long day = 19;
+        System.out.println("Hello " + name);
+        System.out.println("miesiąc :" + month);
+        System.out.println("rok :" + year);
+        System.out.println("dzień :" + day);
+    }
+}
